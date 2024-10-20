2a31ea66a8a3b82605fe05f6aa5bfe36761e57cb
fatima zahra sabbar <fatimazahra20033@gmail.com>
2024-10-20 12:12:28 +0100

    Add existing project files to Git

--- app/api/newsletter/route.js
+++ app/api/newsletter/route.js
@@ -4,13 +4,11 @@ import nodemailer from 'nodemailer';
 
 
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const MY_EMAIL = process.env.MY_EMAIL;
 
-const ***REMOVED***
-const ***REMOVED***
-const ***REMOVED***
-const ***REMOVED***
-
-const ***REMOVED***
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
--- app/api/packages/route.js
+++ app/api/packages/route.js
@@ -6,12 +6,11 @@ import nodemailer from 'nodemailer';
 
 
 
-const ***REMOVED***
-const ***REMOVED***
-const ***REMOVED***
-const ***REMOVED***
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const MY_EMAIL = process.env.MY_EMAIL;
 
-const ***REMOVED***
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
